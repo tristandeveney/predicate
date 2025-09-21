478caa386b18f3e05de3716bc2ab32de0ac5391c
tristandeveney <164981629+tristandeveney@users.noreply.github.com>
2025-09-21 08:51:14 -0700

    sitemap, trailing slash for search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({ site: 'https://www.predicatehighered.com', });
+export default defineConfig({
+    site: 'https://www.predicatehighered.com',
+
+    trailingSlash: 'always',
+
+    integrations: [
+        sitemap({
+            canonical: true,
+        }),
+    ],
+});
